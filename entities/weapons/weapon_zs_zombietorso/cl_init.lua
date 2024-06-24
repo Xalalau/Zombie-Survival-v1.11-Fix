307873f7500b2b124e0024eafcff38e0b49c4eb4
Xalalau <xalalau@outlook.com>
2024-06-24 04:36:50 -0300

    Fix zombie torso viewmodel arms pos

--- entities/weapons/weapon_zs_zombietorso/cl_init.lua
+++ entities/weapons/weapon_zs_zombietorso/cl_init.lua
@@ -3,7 +3,7 @@ include("shared.lua")
 SWEP.PrintName = "Zombie Torso"
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = true
-SWEP.ViewModelFOV = 70
+SWEP.ViewModelFOV = 90
 SWEP.ViewModelFlip = false
 SWEP.CSMuzzleFlashes = false
 
@@ -26,4 +26,12 @@ function SWEP:DrawWeaponSelection(x, y, wide, tall, alpha)
 	draw.SimpleText(self.PrintName, "HUDFontSmallAA", x + wide * 0.5, y + tall * 0.5, COLOR_RED, TEXT_ALIGN_CENTER)
 	draw.SimpleText(self.PrintName, "HUDFontSmallAA", XNameBlur2 + x + wide * 0.5, YNameBlur + y + tall * 0.5, color_blur1, TEXT_ALIGN_CENTER)
 	draw.SimpleText(self.PrintName, "HUDFontSmallAA", XNameBlur + x + wide * 0.5, YNameBlur + y + tall * 0.5, color_blu1, TEXT_ALIGN_CENTER)
+end
+
+-- Custom view model position - Xala
+function SWEP:CalcViewModelView(ViewModel, oldPos, oldAng, pos, ang)
+    local newPos = pos + Vector(0, 0, -35)
+    local newAng = ang
+
+    return newPos, newAng
 end
